48122357b1d5e9060a33d9ef476c1f0819728cd4
Chris <cjmarsh725@gmail.com>
2018-11-26 17:21:42 -0800

    created test card content and added props to column components

--- board/src/components/Board/Board.js
+++ board/src/components/Board/Board.js
@@ -6,17 +6,22 @@ class Board extends Component {
   constructor(props) {
     super(props);
     this.state = {
-  
+      cards = [
+        ["Test Card 1", "Test Card 2", "Test Card 3"],
+        ["Test Card 1", "Test Card 2", "Test Card 3"],
+        ["Test Card 1", "Test Card 2", "Test Card 3"],
+        ["Test Card 1", "Test Card 2", "Test Card 3"]
+      ]
     };
   }
 
   render() {
     return (
       <div className="board-container">
-        <Column title="Column 1" />
-        <Column title="Column 2" />
-        <Column title="Column 3" />
-        <Column title="Column 4" />
+        <Column title="Column 1" index="0" cards={this.state.cards[0]} />
+        <Column title="Column 2" index="1" cards={this.state.cards[1]} />
+        <Column title="Column 3" index="2" cards={this.state.cards[2]} />
+        <Column title="Column 4" index="3" cards={this.state.cards[3]} />
       </div>
     )
   }
