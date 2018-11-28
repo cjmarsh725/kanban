20eee31384774000bae71be72e3930a638a65a6d
Chris <cjmarsh725@gmail.com>
2018-11-27 18:50:55 -0800

    swap card function

--- board/src/components/Board/Board.js
+++ board/src/components/Board/Board.js
@@ -15,13 +15,21 @@ class Board extends Component {
     };
   }
 
+  swapCards = (destColIndex, colIndex, cardIndex) => {
+    const cards = this.state.cards;
+    const cardContent = cards[colIndex][cardIndex];
+    cards[colIndex].splice(cardIndex, 1);
+    cards[destColIndex].push(cardContent);
+    this.setState({cards});
+  }
+
   render() {
     return (
       <div className="board-container">
-        <Column title="Column 1" index="0" cards={this.state.cards[0]} />
-        <Column title="Column 2" index="1" cards={this.state.cards[1]} />
-        <Column title="Column 3" index="2" cards={this.state.cards[2]} />
-        <Column title="Column 4" index="3" cards={this.state.cards[3]} />
+        <Column title="Column 1" index="0" cards={this.state.cards[0]} swap={this.swapCards} />
+        <Column title="Column 2" index="1" cards={this.state.cards[1]} swap={this.swapCards} />
+        <Column title="Column 3" index="2" cards={this.state.cards[2]} swap={this.swapCards} />
+        <Column title="Column 4" index="3" cards={this.state.cards[3]} swap={this.swapCards} />
       </div>
     )
   }
