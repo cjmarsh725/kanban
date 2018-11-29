8005b46ed194abeebc9053dfb9d053ed70055420
Chris <cjmarsh725@gmail.com>
2018-11-28 18:03:51 -0800

    swapping button initial implementation

--- board/src/components/Card/Card.js
+++ board/src/components/Card/Card.js
@@ -4,7 +4,9 @@ import './Card.css'
 export default function Card(props) {
   return (
     <div className="card-container">
+      <a href="#" class="swap-card">&#8249;</a>
       <p>{props.content}</p>
+      <a href="#" className="swap-card">&#8250;</a>
     </div>
   );
 }
